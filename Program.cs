8b695f0a0957d0d33e0bae3dec7dc10bf547cadd
Jaybhaye Sachin <114004126+jaybhayesachin@users.noreply.github.com>
2024-11-09 21:43:54 +0530

    Add files via upload
    Array1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DsaProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =new int[5];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("enter itm aary");
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i=0; i< arr.Length;i++)
+                {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
